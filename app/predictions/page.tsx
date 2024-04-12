1c8c09d2fcc288ad3e9b8f5648a01eb58a639e97
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-04-12 03:59:54 +0100

    Initial

--- app/predictions/page.tsx
+++ app/predictions/page.tsx
@@ -1,7 +1,9 @@
-const Predictions = () => {
-  return (
-    <div>Predictions</div>
-  )
-}
+import Prediction from '@/scenes/prediction/Prediction';
+import { getKpiData } from '../action';
 
-export default Predictions
+export default async function Predictions() {
+  const KPIsData = await getKpiData();
+  
+
+  return <Prediction kpiData={KPIsData} />;
+}
